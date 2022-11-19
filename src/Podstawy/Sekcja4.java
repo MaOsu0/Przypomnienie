6b644430ebe6afda2edf054d06a9b3f6e0a7412c
MaOsu0 <mariusz.osuch1@gmail.com>
2022-11-19 08:24:00 +0100

    19.11.2022

---
+++ src/Podstawy/Sekcja4.java
@@ -0,0 +1,31 @@
+package Podstawy;
+
+class Sekcja4 {
+    public static void main(String[] args) {
+
+
+        String firstName = "Arek";
+        String lastName = "Włodarczyk";
+        long pesel = 90010112345L;
+        int howOld = 18;
+        long c;
+
+        short liczba = 127;
+
+        c = howOld + pesel;
+        System.out.println("c");
+        //Stwórz zmienną dwuczłonową (składającą się z dwóch słów), samoopisującą się.
+        String zmiennaDwuczłonowa = "Zmienna dwuczłonowa";
+
+        // Stwórz zmienne:
+        //imię
+        //nazwisko
+        //wiek
+
+        System.out.println(firstName + " " + lastName + " " + "lat " + howOld);
+
+        final double LICZBA_PI = 3.14;
+        System.out.println(LICZBA_PI);
+
+    }
+}
